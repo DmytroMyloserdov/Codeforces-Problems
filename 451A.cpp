f066c65e04563a6c310a52bb0c79849c4b7a1e15
Bodnaralex <saintdevil1132@gmail.com>
2019-01-21 11:41:54 +0200

    .

--- 451A.cpp
+++ 451A.cpp
@@ -4,13 +4,19 @@ using namespace std;
 int main()
 {
 	int n, m; cin >> n >> m;
+	int n1 = n, m1 = m, who = 0;
 	
-	if (n == 1 || m == 1){
-		cout << "Akshat";
-		return 0;
+	while (n1 * m1 > 0)
+	{
+		who++;
+		n1--;
+		m1--;
 	}
-	if ((m * n) % 2 != 0) cout << "Akshat";
-	else cout << "Malvika";
 		
+	if (who % 2 == 0)
+		cout << "Malvika";
+	else
+		cout << "Akshat";	
+	
 	
 }
